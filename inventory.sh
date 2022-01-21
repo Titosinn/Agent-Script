7bc7a4d98fe540ce5fe2bd9185109fec4e04aea6
Titosin123 <74934023+Titosinn@users.noreply.github.com>
2022-01-21 14:51:21 -0600

    Update inventory.sh

--- inventory.sh
+++ inventory.sh
@@ -3,10 +3,29 @@
 #Author: Titosin
 #Description: Script for a system inventory
 
-top
-free
-uname -r
-lsblk
+echo -e "\nNumber of Cpus:\n"
+sleep 3
 nproc
 
-echo "All commands ran successfully"
+echo -e "\nRAM or Memory Size:\n"
+sleep 3
+free
+
+echo -e "\nHard disk size:\n"
+sleep 3
+lsblk
+
+echo -e "\nKernel Version:\n"
+sleep 3
+uname -r
+
+echo -e "\nOS Version:\n"
+sleep 3
+cat /etc/*release
+
+
+echo -e "\nOS or System Bits:\n"
+sleep 3
+uname -m
+
+echo -e "\nAll commands ran successfully\n"
